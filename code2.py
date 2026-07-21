d7274e64e901e25869283973cffd3d108238be04
Ved Vyas <vedcodes2312@gmail.com>
2026-07-21 07:24:01 +0530

    Update code2.py

--- code2.py
+++ code2.py
@@ -8,3 +8,7 @@ print("Adjustment type:", adjustment.dtype)
 
 print("Temperatures shape:", temperatures.shape)
 print("Temperatures type:", temperatures.dtype)
+
+# Add the temperatures and adjustment tensors
+corrected_temperatures = temperatures + adjustment
+print("Corrected temperatures:", corrected_temperatures)
