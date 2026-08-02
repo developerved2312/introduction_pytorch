cf015a271d53271c71c67828df1d828cc1436136
Ved Vyas <vedcodes2312@gmail.com>
2026-08-02 20:48:04 +0530

    Create code5.py

---
+++ code5.py
@@ -0,0 +1,14 @@
+import torch
+from torch.utils.data import TensorDataset
+
+X = animals.iloc[:, 1:-1].to_numpy()  
+Y = animals.iloc[:, -1].to_numpy()
+
+# Create a dataset
+dataset = TensorDataset(torch.tensor(X), torch.tensor(Y))
+
+# Print the first sample
+input_sample, label_sample = dataset[0]
+print('Input sample:', input_sample)
+print('Label sample:', label_sample)
+
